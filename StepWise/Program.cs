65a920a902df99b6bd6db9dea1d18c584b927215
Irena07 <irra.nalbantova@gmail.com>
2025-05-19 19:58:57 +0300

    Connection string renamed

--- StepWise/Program.cs
+++ StepWise/Program.cs
@@ -5,7 +5,7 @@ using StepWise.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("StepWiseDbConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<StepWiseDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
