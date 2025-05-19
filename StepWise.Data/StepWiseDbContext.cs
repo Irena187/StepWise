d61087a1fd386f9b4e677f6adb68021f5dabf965
Irena07 <irra.nalbantova@gmail.com>
2025-05-19 20:19:16 +0300

    Type of key user changed to GUID

--- StepWise.Data/StepWiseDbContext.cs
+++ StepWise.Data/StepWiseDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using StepWise.Data.Models;
 
 namespace StepWise.Data
 {
-    public class StepWiseDbContext : IdentityDbContext
+    public class StepWiseDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public StepWiseDbContext(DbContextOptions<StepWiseDbContext> options)
             : base(options)
--- StepWise/Program.cs
+++ StepWise/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StepWise.Data;
+using StepWise.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<StepWiseDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<StepWiseDbContext>();
 builder.Services.AddControllersWithViews();
 
